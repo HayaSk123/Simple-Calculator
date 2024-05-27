525a693bf2a5b5bd3e231e0cb900884fd55c7a4b
HayaSk123 <154345035+HayaSk123@users.noreply.github.com>
2024-05-27 11:35:54 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,35 +1,149 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{
-    type: "number",
-    name: "num1",
-    message: "Enter the first number"
-},
-{
-    type: "number",
-    name: "num2",
-    message: "Enter the second number"
-},
-{
-    type: "list",
-    name: "operator",
-    message: "Which operation would you like to perform",
-    choices: ["Addition","Subtraction","Multiplication","Division"]
+
+let currentBalance: number = 10000;
+const pin = 12345;
+
+console.log(
+  chalk.red.bold("<-------  Welcome to Haya Sikander's ATM  ------->")
+);
+
+async function promptPin(): Promise<number> {
+  let response = await inquirer.prompt([
+    {
+      type: "number",
+      name: "pincode",
+      message: "Enter your pin number",
+    },
+  ]);
+  return response.pincode;
 }
-])
-if(answer.operator == "Addition")
+
+async function promptOperation(): Promise<string> {
+  let response = await inquirer.prompt([
     {
-        console.log(`${answer.num1} + ${answer.num2} = `, answer.num1 + answer.num2)
-    }
-else if(answer.operator == "Subtraction")
-    {
-        console.log(`${answer.num1} - ${answer.num2} = `, answer.num1 - answer.num2)
-    }
-else if(answer.operator == "Multiplication")
-    {
-        console.log(`${answer.num1} * ${answer.num2} = `, answer.num1 * answer.num2)
-    }
-else 
-{
-    console.log(`${answer.num1} / ${answer.num2} = `, answer.num1 / answer.num2)
+      type: "list",
+      name: "operation",
+      message: "What would you like to do?",
+      choices: ["Deposit", "Withdraw", "Check Balance"],
+    },
+  ]);
+  return response.operation;
 }
+
+async function promptAmount(message: string): Promise<number> {
+  let response = await inquirer.prompt([
+    {
+      type: "number",
+      name: "amount",
+      message: message,
+    },
+  ]);
+  return response.amount;
+}
+
+async function promptYesNo(message: string): Promise<string> {
+  let response = await inquirer.prompt([
+    {
+      type: "list",
+      name: "question",
+      message: message,
+      choices: ["Yes", "No"],
+    },
+  ]);
+  return response.question;
+}
+
+async function handleDeposit() {
+  let amount = await promptAmount("What amount would you like to deposit?");
+  currentBalance += amount;
+  console.log(chalk.green(`Your amount was successfully deposited`));
+  if (
+    (await promptYesNo("Would you like to check your current balance?")) ===
+    "Yes"
+  ) {
+    console.log(chalk.blue(`Your current balance is ${currentBalance}`));
+  } else {
+    console.log(chalk.red("Thank you for using our ATM!"));
+  }
+}
+
+async function handleWithdrawal() {
+  let amount = await promptAmount("What amount would you like to withdraw?");
+  if (amount <= currentBalance) {
+    currentBalance -= amount;
+    console.log(chalk.green(`Your withdrawal was successful`));
+  } else {
+    console.log(chalk.red("You don't have enough money in your account!"));
+    if ((await promptYesNo("Would you like to apply for a loan?")) === "Yes") {
+      let loanAmount = await promptAmount("How much would you like to borrow?");
+      currentBalance += loanAmount;
+      console.log(
+        chalk.green(
+          `Your loan has been approved and your current balance is ${currentBalance}`
+        )
+      );
+      if (
+        currentBalance >= amount &&
+        (await promptYesNo(
+          "Would you like to continue your process of withdrawal?"
+        )) === "Yes"
+      ) {
+        currentBalance -= amount;
+        console.log(chalk.green("Your withdrawal was successful"));
+      } else {
+        console.log(chalk.red("Your transaction was cancelled!"));
+      }
+    } else {
+      console.log(chalk.red.bold("Withdrawal unsuccessful!"));
+    }
+  }
+
+  if (
+    (await promptYesNo("Would you like to check your current balance?")) ===
+    "Yes"
+  ) {
+    console.log(chalk.blue(`Your current balance is ${currentBalance}`));
+  } else {
+    console.log(chalk.red("Thank you for using our ATM!"));
+  }
+}
+
+async function main() {
+  let pincode = await promptPin();
+
+  if (pincode === pin) {
+    console.log(chalk.green.bold("Your pin is correct"));
+    let operation = await promptOperation();
+
+    if (operation === "Deposit") {
+      await handleDeposit();
+    } else if (operation === "Withdraw") {
+      await handleWithdrawal();
+    } else {
+      console.log(chalk.blue(`Your current balance is ${currentBalance}`));
+    }
+  } else {
+    console.log(chalk.red("Invalid input! Please try again."));
+    pincode = await promptPin();
+
+    if (pincode === pin) {
+      console.log(chalk.green.bold("Your pin is correct"));
+      let operation = await promptOperation();
+
+      if (operation === "Deposit") {
+        await handleDeposit();
+      } else if (operation === "Withdraw") {
+        await handleWithdrawal();
+      } else {
+        console.log(chalk.blue(`Your current balance is ${currentBalance}`));
+      }
+    } else {
+      console.log(chalk.red("Invalid input! Login unsuccessful"));
+    }
+  }
+}
+
+main();
+
